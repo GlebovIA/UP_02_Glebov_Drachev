28e1c90a7429aff23dbf859e3f2181446508fc73
GlebovIA <kobelevadara210@mail.com>
2025-02-14 17:20:34 +0500

    Дополнение моделей

--- Models/Marks.cs
+++ Models/Marks.cs
@@ -9,9 +9,47 @@ namespace UP_02_Glebov_Drachev.Models
     public class Marks
     {
         private int id;
+        private DateTime date;
         private string mark;
         private int discipline;
         private int teacher;
         private int student;
+        private string description;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        public string Mark
+        {
+            get { return mark; }
+            set { mark = value; }
+        }
+        public int Discipline
+        {
+            get { return discipline; }
+            set { discipline = value; }
+        }
+        public int Teacher
+        {
+            get { return teacher; }
+            set { teacher = value; }
+        }
+        public int Student
+        {
+            get { return student; }
+            set { student = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
     }
 }
--- Models/TeachersLoad.cs
+++ Models/TeachersLoad.cs
@@ -17,5 +17,51 @@ namespace UP_02_Glebov_Drachev.Models
         private int consultationHours;
         private int courceProjectHours;
         private int examHours;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public int Teacher
+        {
+            get { return teacher; }
+            set { teacher = value; }
+        }
+        public int Discipline
+        {
+            get { return discipline; }
+            set { discipline = value; }
+        }
+        public int Group
+        {
+            get { return group; }
+            set { group = value; }
+        }
+        public int LectureHours
+        {
+            get { return lectureHours; }
+            set { lectureHours = value; }
+        }
+        public int PracticeHours
+        {
+            get { return practiceHours; }
+            set { practiceHours = value; }
+        }
+        public int ConsultationHours
+        {
+            get { return consultationHours; }
+            set { consultationHours = value; }
+        }
+        public int CourceProjectHours
+        {
+            get { return courceProjectHours; }
+            set { courceProjectHours = value; }
+        }
+        public int ExamHours
+        {
+            get { return examHours; }
+            set { examHours = value; }
+        }
     }
 }
