fc721618620cc30b0f6f486bd2610c681da2c3f4
Daria <kobelevadara210@mail.com>
2025-02-14 15:28:15 +0500

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace UP_02_Glebov_Drachev
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
