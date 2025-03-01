d2fa03c6b19e7187fd87a9e40d262dd15608bbcc
Stepan <rozkovanstepan@gmail.com>
2025-03-01 16:43:04 +0500

    Начало разработки редактирования консультаций

--- Views/Elements/ConsultationsElement.xaml.cs
+++ Views/Elements/ConsultationsElement.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Controls;
 using System.Windows.Input;
+using UP_02_Glebov_Drachev.Contexts;
 using UP_02_Glebov_Drachev.Models;
 using UP_02_Glebov_Drachev.Views.Pages;
 
@@ -7,19 +8,24 @@ namespace UP_02_Glebov_Drachev.Views.Elements
 {
     public partial class ConsultationsElement : UserControl
     {
-        public ConsultationsElement()
+        public ConsultationsModel Model { get; set; }
+        public ConsultationsContext Context { get; set; }
+        public ConsultationsElement(ConsultationsModel model, ConsultationsContext context)
         {
             InitializeComponent();
+            Model = model;
+            Context = context;
+            DataContext = Model;
         }
 
         private void RedacClick(object sender, MouseButtonEventArgs e)
         {
-            GeneralPage.SwapPages(new ConsultationsPage(DataContext as ConsultationsModel));
+            GeneralPage.SwapPages(new ConsultationsPage(Context, Model));
         }
 
         private void DeleteClick(object sender, MouseButtonEventArgs e)
         {
-            // Логика удаления консультации
+
         }
     }
 }
--- Views/Pages/EntityPages/ConsultationsList.xaml.cs
+++ Views/Pages/EntityPages/ConsultationsList.xaml.cs
@@ -3,21 +3,26 @@ using Microsoft.EntityFrameworkCore;
 using System.Windows.Controls;
 using UP_02_Glebov_Drachev.Contexts;
 using UP_02_Glebov_Drachev.Models;
+using UP_02_Glebov_Drachev.Views.Elements;
+using System.Collections.ObjectModel;
 
 namespace UP_02_Glebov_Drachev.Views.Pages.EntityPages
 {
     public partial class ConsultationsList : Page
     {
+        ConsultationsContext context = new ConsultationsContext();
         public ConsultationsList()
         {
             InitializeComponent();
-            using (var consultationsContext = new ConsultationsContext())
+            var consultations = context.Consultations
+                .Include(c => c.Discipline)
+                .ToList();
+            ObservableCollection<ConsultationsElement> elements = new ObservableCollection<ConsultationsElement>();
+            foreach (ConsultationsModel model in consultations)
             {
-                var consultations = consultationsContext.Consultations
-                    .Include(c => c.Discipline)
-                    .ToList();
-                List.ItemsSource = consultations;
+                elements.Add(new ConsultationsElement(model, context));
             }
+            List.ItemsSource = elements;
         }
 
     }
